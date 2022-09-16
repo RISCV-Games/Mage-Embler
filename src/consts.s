9fcfad916d981b9cd30eaf1e60bc93b8169629f8
Eduardo Freire <dudufreiresantos@gmail.com>
2022-09-16 09:31:41 -0300

    added cursor tiles

--- src/consts.s
+++ src/consts.s
@@ -3,14 +3,14 @@
 .eqv BUFFER_ADRESS 0xFF000000
 .eqv NUMBER_OF_PIXELS 0x12C00
 .eqv TILE_SIZE 16
-.eqv NUMBER_OF_TILES_IN_IMAGE 4
+.eqv NUMBER_OF_TILES_IN_IMAGE 6
 .eqv SCREEN_SIZE 320
 .eqv KBD_CONTROL 0xFF200000
 .eqv CURSOR_IMG tiles
-.eqv CURSOR_NUM0 0 # tile number of first frame of cursor animation
-.eqv CURSOR_NUM1 1 # tile number of second frame of cursor animation
+.eqv CURSOR_NUM0 4 # tile number of first frame of cursor animation
+.eqv CURSOR_NUM1 5 # tile number of second frame of cursor animation
+.eqv CURSOR_ANIM_DELAY 200 # time between each frame of cursor animation
 .eqv SCREEN_CENTER_X 9
 .eqv SCREEN_CENTER_Y 7
 .eqv MAX_WORD 0xFFFFFFFF
 .eqv MIN_WORD 0x10000000
-.eqv CURSOR_ANIM_DELAY 100 # time between each frame of cursor animation
