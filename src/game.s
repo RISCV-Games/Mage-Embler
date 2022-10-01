053ee0294042311051c4b5aba4bf78ae6f1efcd0
Eduardo Freire <dudufreiresantos@gmail.com>
2022-10-01 16:26:52 -0300

    added game(main) file

---
+++ src/game.s
@@ -0,0 +1,22 @@
+.include "../src/data.s"
+.include "../src/macros.s"
+
+.text
+GAME:
+
+jal RUN_GAME_LOGIC
+jal RUN_GAME_RENDER
+
+j GAME
+
+.include "../src/player.s"
+.include "../src/video.s"
+.include "../src/input.s"
+.include "../src/cursor.s"
+.include "../src/map.s"
+.include "../src/utils.s"
+.include "../src/menu.s"
+.include "../src/combat.s"
+.include "../src/dialog.s"
+.include "../src/game_render.s"
+.include "../src/game_logic.s"
